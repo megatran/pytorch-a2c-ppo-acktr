fa823851f483074d9935452d1fff98c54f88d0ac
Nhan Tran <nhantrantnt@gmail.com>
2023-03-29 23:54:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
       # install_requires=['gym', 'matplotlib', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+git://github.com/openai/baselines.git@master#egg=baselines'])
-      install_requires=['gym', 'matplotlib', 'tensorflow<=2.7.0', 'torch<=1.7.1', 'torchvision<=0.8.2'])
+      install_requires=[
+      'gym',
+      'matplotlib',
+      'tensorflow',
+      # 'torch==1.5.0',
+      # 'torchvision==0.6.0',
+      'torch',
+      'torchvision',
+      'baselines @ git+https://github.com/openai/baselines.git#egg=baselines'
+      ])
